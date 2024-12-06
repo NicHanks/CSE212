43fcc4773b79a437010010cfa7e2215d285304c3
NicHanks <104238034+NicHanks@users.noreply.github.com>
2024-12-05 18:20:36 -0700

    this all week 4

---
+++ queue-example/MyThread.cs
@@ -0,0 +1,14 @@
+﻿namespace queue_example;
+
+public class MyThread : BaseThread
+{
+    public MyThread() : base()
+    {
+    }
+
+    public override void RunThread()
+    {
+        // Do some stuff
+        Console.WriteLine("I'm in the thread");
+    }
+}
---
+++ queue-example/QueueList.cs
@@ -0,0 +1,30 @@
+﻿namespace queue_example;
+
+// Implement a queue in using a List
+
+public class QueueList
+{
+    public QueueList()
+    {
+    }
+    
+    public int Size()
+    {
+        return(-1);
+    }
+    
+    public void Enqueue(int value)
+    {
+    }
+    
+    public int Dequeue()
+    {
+        return(-1);
+    }
+
+    public int Peek()
+    {
+        return(-1);
+    }
+    
+}
