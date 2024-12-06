c96a9e6fe5d7180d30b502d6951488e851e4df02
NicHanks <104238034+NicHanks@users.noreply.github.com>
2024-12-05 18:17:29 -0700

    ALL WEEK 9

---
+++ demo-09a/BinarySearchTree.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+
+namespace demo_09a;
+// CSE 212 Lesson 9A
+// Implementation of a basic Binary Search Tree data structure.
+
+public class BinarySearchTree : IEnumerable<int> {
+    private Node? _root;
+
+    /// <summary>
+    /// Insert 'value' into the BST.  If the BST is empty, then
+    /// set the root equal to the new node. Otherwise, use
+    /// Node.Insert to recursively find the location to insert.
+    /// </summary>
+    public void Insert(int value) {
+    }
+
+    /// <summary>
+    /// Yields all values in the tree
+    /// </summary>
+    IEnumerator IEnumerable.GetEnumerator() {
+        // call the typed version of the method
+
+
+
+        
+        return GetEnumerator();
+    }
+
+    /// <summary>
+    /// Iterate forward through the BST
+    /// </summary>
+    public IEnumerator<int> GetEnumerator() {
+        var numbers = new List<int>();
+        TraverseForward(_root, numbers);
+        foreach (var number in numbers) {
+            yield return number;
+        }
+    }
+
+    private void TraverseForward(Node? node, List<int> values) {
+    }
+
+    public override string ToString() {
+        return "<Bst>{" + string.Join(", ", this) + "}";
+    }
+}
---
+++ demo-09a/Node.cs
@@ -0,0 +1,11 @@
+namespace demo_09a;
+
+public class Node {
+    public int Data { get; set; }
+    public Node? Right { get; private set; }
+    public Node? Left { get; private set; }
+
+    public Node(int data) {
+        this.Data = data;
+    }
+}
