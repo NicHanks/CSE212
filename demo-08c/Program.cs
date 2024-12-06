17e58b3616e1b24daa4167ffc821fc0b8f64f1b8
NicHanks <104238034+NicHanks@users.noreply.github.com>
2024-12-05 18:15:14 -0700

    ALL OF WEEK 8

---
+++ demo-08c/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace demo_08c;
+
+public static class Program {
+    private static int Permutations(string letters, string word = "") {
+        // Base Case
+        if (letters.Length == 0) {
+            // If there are no more letters to choose from, then
+            // display the word
+
+            Console.WriteLine(word);
+        }
+        else {
+            // Find each permutation starting with 'word' and adding
+            // each of the remaining letters one at a time.
+            for (int i =0; i < letters.Length; i++)
+            {
+                word = word + letters[i];
+                letters = letters.Remove(i, 1);
+
+                Permutations(letters, word);
+            }
+        }
+
+        return 0;
+    }
+
+    private static void Main() {
+        var result = Permutations("ABCD");
+        // Console.WriteLine(result);
+
+        Console.WriteLine("====================");
+
+        // result = Permutations("ABCDEFG");
+        // Console.WriteLine(result);
+    }
+}
